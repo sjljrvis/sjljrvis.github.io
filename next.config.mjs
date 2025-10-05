98f8044bef1eedb6583cc3e8dabf0baaeb677f3c
sjljrvis <sjlchougule@gmail.com>
2025-10-05 14:13:20 +0530

    Initial Next.js portfolio

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // next.config.js
+  output: 'export',
+  images: {
+    unoptimized: true,
+}
+};
 
 export default nextConfig;
